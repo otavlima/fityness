760e866a14bd304e44fc526031cb23499b7c1506
Otávio Lima <otavio-lima-co@gmail.com>
2026-04-12 18:21:19 -0300

    feat: add register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Login />} path='/login'/>
+      <Route element={<Register />} path='/register'/>
       <Route element={<Home />} index/>
     </Routes>
   )
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -15,20 +15,25 @@ import {
 import { Input } from "@/components/ui/input"
 import { Link } from "react-router-dom"
 
-const Login = () => {
+const Register = () => {
     return (
         <div className="flex flex-col gap-4 items-center justify-center h-screen">
             <h1 className="text-3xl font-bold text-primary">Fitify</h1>
             <div className="w-96 max-[480px]:w-48 h-[1px] bg-gray-200"></div>
             <Card className="max-[480px]:mx-4">
                 <CardHeader className="flex justify-between">
-                   <div>
-                      <CardTitle className="text-xl">Login</CardTitle>
-                      <CardDescription>Login to Fitify and work out with us!</CardDescription>
-                   </div>
-                    <Link className="p-2 rounded-2xl bg-accent hover:bg-gray-200 transition duration-300 cursor-pointer" to="/register">Sign up</Link>
+                    <div>
+                      <CardTitle className="text-xl">Create your account</CardTitle>
+                      <CardDescription>Register to Fitify and work out with us!</CardDescription>
+                    </div>
+                    <Link className="p-2 rounded-2xl bg-accent hover:bg-gray-200 transition duration-300 cursor-pointer" to="/login">Sign in</Link>
                 </CardHeader>
                 <CardContent className="flex flex-col gap-2">
+                     <Field>
+                        <FieldLabel htmlFor="name">Name</FieldLabel>
+                        <Input id="name" autoComplete="off" placeholder="John Doe" />
+                        <FieldDescription>Enter your full name.</FieldDescription>
+                    </Field>
                     <Field>
                         <FieldLabel htmlFor="email">Email</FieldLabel>
                         <Input id="name" autoComplete="off" placeholder="youremail@gmail.com" />
@@ -37,10 +42,14 @@ const Login = () => {
                     <Field>
                         <FieldLabel htmlFor="password">Password</FieldLabel>
                         <Input id="password" autoComplete="off" placeholder="********" />
+                    </Field>
+                     <Field>
+                        <FieldLabel htmlFor="confirm-password">Confirm password</FieldLabel>
+                        <Input id="confirm-password" autoComplete="off" placeholder="Enter your password again" />
                         <FieldDescription>Use a strong password.</FieldDescription>
                     </Field>
                 </CardContent>
-                <Button className="mx-4">Login</Button>
+                <Button className="mx-4">Create Account</Button>
                 <CardFooter>
                     <p>All rights reserved 2026. © Fitify</p>
                 </CardFooter>
@@ -49,4 +58,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Register
